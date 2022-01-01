c73abe84055ba22776616db03027fd3c1eb4dc8b
Tulsi Dey <deytulsi18@gmail.com>
2022-01-01 19:23:12 +0530

    added install button for pwa

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ if ('serviceWorker' in navigator) {
         .then(reg => console.log('service worker registered'))
         .catch(err => console.log('service worker not registered', err));
 }
+
+window.addEventListener('beforeinstallprompt', e => {
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+        return e.preventDefault();
+    } else {
+        const btn = document.querySelector('#install')
+        btn.hidden = false;
+        btn.onclick = _ => e.prompt();
+        return e.preventDefault();
+    }
+});
